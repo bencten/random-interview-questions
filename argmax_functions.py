d7e0a44a27cef94d59dabf701e9f75ebbb92ed84
Benjamin Cichy <benjamin.cichy@gmail.com>
2016-04-13 23:20:59 -0700

    Reorganized questions

--- argmax_functions.py
+++ argmax_functions.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+#write a function that will return the index of the highest value in an array, arr
+#arr = [-1,0,5,1,5,3,4,3]
+
 
 def argmax1(arr):
     ''' Returns the index from the largest item in an array
@@ -9,6 +14,11 @@ def argmax1(arr):
         if largest_int == i:
             return a
 
+
+
 if __name__ == '__main__':
     arr = [-1,0,5,1,5,3,4,3]
-    print argmax(arr)
+
+    #reference answer from numopyp library
+    print np.argmax(arr)
+    print argmax1(arr)
